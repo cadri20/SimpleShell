671a1cc2c360da5e77c638ed5f325d93b38217b1
Adrian <cadrian_2001@hotmail.com>
2020-11-08 10:22:31 -0500

    Name changed

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include "utils.h"
 int main(){
 	while(1){
-		cout << "simple shell > ";
+		cout << "SimpleShell > ";
 		char *arrayEntry[20];
 		int argc = argsToArray(arrayEntry);
 		Command cmdIn(arrayEntry[0],arrayEntry,argc);
