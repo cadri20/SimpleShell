e6751e19295fca918027f9d5543399d6ff082e24
Adrian <cadrian_2001@hotmail.com>
2020-11-08 09:41:46 -0500

    utils added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "Command.h"
 #include <unistd.h>
-int main(){
+#include <iostream>
+using namespace std;
+#include "utils.h"
+int main(int argc, char *argv[]){
 	char * args[] = {"ls","-l"};
-	Command cmd("ls",args,2);	
+	cout << "Argc = " << argc << endl;
+	Command cmd(args[0],args,2);	
+	//Command cmdIn(argv[0],argv,argc);
 	cmd.printArgs();
+	//cmdIn.printArgs();
 	cmd.run();
+	char *arrayEntry[20];
+	argsToArray(arrayEntry);
+	printArray(arrayEntry[0],3);
 }
---
+++ utils.h
@@ -0,0 +1,2 @@
+void argsToArray(char *[]);
+void printArray(char*,int);
